49ea68df7003bca48d72d9a581323b33c146b984
alexandrosbouzalas <alexandrosbouzalas@outlook.com>
2022-10-01 00:59:07 +0200

    # fetch crypto data function

--- public/js/home.js
+++ public/js/home.js
@@ -8,3 +8,19 @@ $('.tab').click((event) => {
 $('#exit-btn').click(() => {
     window.location.pathname = '/logout';
 })
+
+const fetchCryptoData = () => {
+    $.ajax({
+        url: "https://rest.coinapi.io/v1/assets",
+        method: "GET",
+        contentType: "application/json",
+        headers: {'X-CoinAPI-Key': 'FECE57DA-544D-425B-92E5-DDA3AC296063'},
+        success: function (response) {
+          console.log(response)
+        },
+        error: function (err) {
+            // Insert error handling here
+          console.log(err.responseJSON.error)
+        },
+      });
+}
